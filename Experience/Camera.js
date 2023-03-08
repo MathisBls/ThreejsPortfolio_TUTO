0eb37db772566656f3e4bbf07e45ec3a5998680e
Mathis Boulais <https://github.com/Asukyy>
2023-03-08 10:07:22 +0100

    first commit

---
+++ Experience/Camera.js
@@ -0,0 +1,36 @@
+import * as THREE from "three";
+import Experience from "./Experience.js";
+
+export default class Camera {
+  constructor() {
+    this.experience = new Experience();
+    this.sizes = this.experience.sizes;
+    this.scene = this.experience.scene;
+    this.canvas = this.experience.canvas;
+
+    this.createPerspectiveCamera();
+    this.creatorOrthographicCamera();
+  }
+
+  createPerspectiveCamera() {
+    this.createPerspectiveCamera = new THREE.PerspectiveCamera(
+        35,
+        this.sizes.aspect, 
+        0.1,
+        1000
+    );
+        this.scene.add(this.perspectiveCamera)
+}
+creatorOrthographicCamera() {
+    this.frustrum = 5
+    this.orthographicCamera = new THREE.OrthographicCamera(
+        (-this.sizes.aspect * this.sizes.frustrum) / 2,
+        (this.sizes.aspect * this.sizes.frustrum) / 2,
+        this.sizes.frustrum / 2,
+        -this.sizes.frustrum / 2,
+        -100,
+        100
+    );
+        this.scene.add(this.orthographicCamera)
+}
+}
